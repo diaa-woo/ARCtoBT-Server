ba1b248cafcc6e8663ea9c8ecd51edb101251d0e
diadntjr <dev.dntjr0501@gmail.com>
2023-07-21 21:57:04 +0900

    Update server
    Add post request

--- server.py
+++ server.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, json
 import subprocess
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "뭐"
+
+@app.route('/login', methods=['POST'])
+def login():
+    if len(request.get_data()) == 0:
+        return 'null'
+    params = json.loads(request.get_data(), encoding='utf-8')
+   
+    return params
 
 if __name__ == '__main__':
     wlan_address = subprocess.check_output(["hostname", "-I"]).decode().split()[0]      # hostname -I를 통해 파악 후, 상황에 따라 인덱스 값 변경
