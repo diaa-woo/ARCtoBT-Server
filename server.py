f0023e496c431e23ea0fd2e69321c5a6d5988705
diadntjr <dev.dntjr0501@gmail.com>
2023-07-22 01:38:14 +0900

    Refactoring server
    refactoring subprocess
        - Change check_output -> run

--- server.py
+++ server.py
@@ -21,5 +21,5 @@ def login():
         return '?'
 
 if __name__ == '__main__':
-    wlan_address = subprocess.check_output(["hostname", "-I"]).decode().split()[0]      # hostname -I를 통해 파악 후, 상황에 따라 인덱스 값 변경
+    wlan_address = subprocess.run(["hostname", "-I"], capture_output=True).stdout.decode().split()[0]     # hostname -I를 통해 파악 후, 상황에 따라 인덱스 값 변경
     app.run(host=wlan_address, debug=True)       
